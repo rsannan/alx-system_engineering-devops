3e5a3940df9994f05a64a29fbb107db5dab886ff
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-11-14 22:25:39 +0000

    Add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+for a given employee ID, returns information about his/her TODO list progress
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    eid = sys.argv[1]
+    employee_req = requests.get('{}users/{}'.format(url, eid))
+    employee_list = employee_req.json()
+    employee_name = employee_list.get('name')
+    todo_list = requests.get('{}todos'.format(url))
+    all_todos = todo_list.json()
+    todo = 0
+    done = 0
+    for item in all_todos:
+        if item.get('userId') == int(eid):
+            todo += 1
+            if item.get('completed') is True:
+                done += 1
+    print('Employee {} is done with tasks({}/{}):'.format(
+                                                  employee_name,
+                                                  done, todo))
+
+    for item in all_todos:
+        if item.get('userId') == int(eid) and item.get('completed') is True:
+            print('\t {}'.format(item.get('title')))
+
+    username = employee_list.get('username')
+    for item in all_todos:
+        if item.get('userId') == int(eid):
+            with open('{}.csv'.format(eid), 'a', encoding='UTF8') as f:
+                row = '"{}","{}","{}","{}"\n'.format(
+                            eid,
+                            username,
+                            item.get('completed'),
+                            item.get('title'))
+                f.write(row)
