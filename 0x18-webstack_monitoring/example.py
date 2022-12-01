1cc2baf798e755ea2fb5ce4c610bb4727f3e89aa
rsannan <shawnrannan@gmail.com>
2022-12-01 02:54:11 +0000

    Add web dubugging 3

---
+++ 0x18-webstack_monitoring/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Get all hosts for your organization returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.hosts_api import HostsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = HostsApi(api_client)
+    response = api_instance.list_hosts()
+
+    print(response)
